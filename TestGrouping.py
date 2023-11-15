eae6d977268ffd65e3312b0d82d58bf11e78a38d
Ghdeer Abuoda <dewet.sends@gmail.com>
2023-11-15 10:52:07 -0500

    Templating

--- TestGrouping.py
+++ TestGrouping.py
@@ -3,9 +3,23 @@ import pandas as pd
 df = pd.read_csv('data/train_job.csv', header=None)
 
 #%%
+import pandas as pd
+# Load the CSV file into a Pandas data frame
+df = pd.read_csv('data/train_job.csv',header=None, delimiter='#')
+# Print the data frame
+print(df)
+
+#%%
 df[30] = df[29].str.split('#').apply(pd.Series)[2]
 df[30] = df[30].astype(float)
 df_clean = df.dropna()
+#%%
+
+from sklearn.cluster import KMeans
+model = KMeans(n_clusters=15)
+classes = model.fit(df[[4]])
+classes.labels_
+
 #%%
 from sklearn.cluster import KMeans
 model = KMeans(n_clusters=15)
